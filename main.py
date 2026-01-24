279a4c00b264a2ca7f69d20fe0ecfc5b5cc5dad1
Ron Ih <ronih@pacbell.net>
2026-01-24 01:11:17 -0800

    Added token counts

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
         model="gemini-2.5-flash",
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
+    if response.usage_metadata == None:
+        raise RuntimeError("Usage metadata is None")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
