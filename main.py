d9715f4ec5d957710cf3a220c835e129c4e63fc9
Ron Ih <ronih@pacbell.net>
2026-01-26 22:56:43 -0800

    Added changes for multiple messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,10 +13,10 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot using Gemini API")
     parser.add_argument("user_prompt", type=str, help="The prompt to send to the chatbot")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=args.user_prompt
-    )
+        model="gemini-2.5-flash", contents=messages
+)
     if response.usage_metadata == None:
         raise RuntimeError("Usage metadata is None")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
