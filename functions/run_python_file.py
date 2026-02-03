c389af33d1b7cc9914a885b19cce94a09aec1daf
Ron Ih <ronih@pacbell.net>
2026-02-03 00:39:24 -0800

    Added run_python_file.py and test_run_python_file.py

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -8,19 +8,20 @@ def run_python_file(working_directory, file_path, args=None):
         target_file = os.path.normpath(target_file)
         valid_target_file = os.path.commonpath([abs_dir, target_file]) == abs_dir
         if not valid_target_file:
-            return f"Error: Cannot run \"{file_path}\" as it is outside the permitted working directory"
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         valid_file = os.path.isfile(target_file)
         if not valid_file:
-            return f'Error: File not found or is not a regular file: \"{file_path}\"'
-        
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
         command = ["python", target_file]
         if args:
             command.extend(args)
-
-        result = subprocess.run(command, capture_output=True, text=True)
+        result = subprocess.run(command, capture_output=True, text=True, timeout=30)
         if result.returncode != 0:
-            return f"Error: {result.stderr.strip()}"
-        
-        return result.stdout.strip()
+            return f"Process exited with code {result.returncode}"
+        if result.stdout is None and result.stdout.strip() == "":
+            return "No output produced"
+        return f"STDOUT: {result.stdout.strip()}. STDERR: {result.stderr.strip()}"
     except Exception as e:
         return f"Error: {str(e)}"
