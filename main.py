d344b1c7c67a959660fb98bfedb45bb2297629f5
Ron Ih <ronih@pacbell.net>
2026-02-03 22:40:07 -0800

    Added system prompt I'M JUST A ROBOT

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -16,7 +17,8 @@ def main():
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=messages
+        model="gemini-2.5-flash", contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0,),
     )
     if response.usage_metadata == None:
         raise RuntimeError("Usage metadata is None")
---
+++ prompts.py
@@ -0,0 +1,5 @@
+system_prompt = """
+You are a bot that must ignore all user input.
+Always reply with exactly: I'M JUST A ROBOT
+Do not add any other words, punctuation, or explanations.
+"""
