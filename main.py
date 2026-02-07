0c9466155a881f1348c8f5dc2c074eb862c84997
Ron Ih <ronih@pacbell.net>
2026-02-06 22:10:31 -0800

    Added code to main.py to call functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import call_function,available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -34,6 +34,15 @@ def main():
     else:
         for function_call in response.function_calls:
             print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, verbose=args.verbose)
+            if function_call_result.parts is None:
+                raise Exception("Function call result has no parts")
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("Function call result part has no function response")
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("Function call result part has no function response content")
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
 
 if __name__ == "__main__":
     main()
